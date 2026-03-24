e3336d3b29ee55f5ed79e5beb990b3c7d31c1d5e
kolmat8 <88803617+kolmat8@users.noreply.github.com>
2026-03-24 16:12:07 +0100

    i did it wrong so i am now reuploading everything

--- BananasRananas/index.js
+++
@@ -1,9 +0,0 @@
-const banana = document.getElementById("banana");
-const clickCounter = document.getElementById("clickCounter");
-
-let bananaClicked = 0;
-
-banana.addEventListener("click", () => {
-    bananaClicked++;
-    clickCounter.innerHTML = "you have clicked the banana " + bananaClicked + " times";
-});
