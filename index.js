1bb502fe363029bf8d2334b65faa26cdb8d81557
kolmat8 <88803617+kolmat8@users.noreply.github.com>
2026-03-28 02:12:39 +0100

    added 67 jumpscare

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const banana = document.getElementById("banana");
 const clickCounter = document.getElementById("clickCounter");
+var scream = new Audio('hl2-stalker-scream.mp3');
 
 let bananaClicked = 0;
+
+function trog() {
+    if (bananaClicked == 67) {
+        sceam.play()
+    }
+}
 
 banana.addEventListener("click", () => {
     bananaClicked++;
     clickCounter.innerHTML = "you have clicked the banana " + bananaClicked + " times";
+    if (bananaClicked == 67) {
+        setTimeout(trog, 6.7)
+    }
 });
