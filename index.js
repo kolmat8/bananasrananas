bea43840bf72568037058133c164d666cc0496e0
kolmat8 <88803617+kolmat8@users.noreply.github.com>
2026-03-28 14:28:58 +0100

    added click sound functionality

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const banana = document.getElementById("banana");
 const clickCounter = document.getElementById("clickCounter");
 var scream = new Audio('hl2-stalker-scream.mp3');
+var click = new Audio('click.mp3');
 
 let bananaClicked = 0;
 
 scream.play().then(() => {
     scream.pause();
     scream.currentTime = 0;
+}).catch(() => {});
+click.play().then(() => {
+    click.pause();
+    click.currentTime = 0;
 }).catch(() => {});
 
 function trog() {
@@ -17,6 +22,9 @@ function trog() {
 
 banana.addEventListener("click", () => {
     bananaClicked++;
+    scream.pause();
+    scream.currentTime = 0;
+    click.play()
     clickCounter.innerHTML = "you have clicked the banana " + bananaClicked + " times";
     if (bananaClicked == 67) {
         setTimeout(trog, 6700)
